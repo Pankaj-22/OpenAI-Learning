3fe853f676383caaa48cba5fe689f3fd541c10f7
pratikshaShinde8037 <pratikshashinde8037@gmail.com>
2024-02-12 19:12:39 +0530

    added key

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import openai
 
 # Create, copy, and paste your API key here:
-openai.api_key = "sk-123456789"
+openai.api_key = "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"
 
 response = openai.Completion.create(model="text-davinci-003",prompt="2+2=",temperature=0, max_tokens=10)
 
